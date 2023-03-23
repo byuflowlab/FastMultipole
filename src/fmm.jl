9091586348774aa11e97d390e2ab9ed5734d801e
Ryan Anderson <rymanderson@gmail.com>
2023-03-22 18:23:26 -0600

    specify sources and targets outside of options

--- src/fmm.jl
+++ src/fmm.jl
@@ -86,111 +86,13 @@ function downward_pass!(tree, elements, j_source, targets_index)
 end
 
 #####
-##### swapping sources and targets; treats sources like targets and vice versa
-#####
-function P2P_swapped!(tree, elements_tuple::Tuple, i_target, j_source, targets_index, sources_index)
-    target_branch = tree.branches[i_target]
-    source_branch = tree.branches[j_source]
-    for (ii_source,source_elements) in enumerate(elements_tuple[targets_index])
-        i_source = targets_index[ii_source]
-        for (ii_target,target_elements) in enumerate(elements_tuple[sources_index])
-            i_target = sources_index[ii_target]
-            is_target = target_branch.first_body[i_target]:target_branch.first_body[i_target]+target_branch.n_bodies[i_target]-1
-            target_positions = target_elements.bodies[i_POSITION,is_target]
-            target_potential = view(target_elements.potential, :, is_target)
-            is_source = source_branch.first_body[i_source]:source_branch.first_body[i_source]+source_branch.n_bodies[i_source]-1
-            sources = source_elements.bodies[:,is_source]
-            source_elements.direct!(target_potential, target_positions, sources)
-        end
-    end
-end
-
-function upward_pass_swapped!(tree::Tree, elements, targets_index)
-    upward_pass_swapped!(tree, elements, 1, targets_index)
-end
-
-function upward_pass_swapped!(tree, elements, i_branch, targets_index)
-    # recursively iterate through branches
-    branch = tree.branches[i_branch]
-    for i_child in branch.first_branch:branch.first_branch + branch.n_branches-1
-        upward_pass_swapped!(tree, elements, i_child, targets_index)
-    end
-
-    if branch.is_target
-        # perform P2M (leaf level) or M2M (not leaf level) translations
-        if branch.first_branch == -1 # leaf level
-            B2M!(tree, elements, i_branch, targets_index)
-        else
-            M2M!(tree, i_branch) # not leaf level
-        end
-    end
-end
-
-function horizontal_pass_swapped!(tree, elements::Tuple, theta, targets_index, sources_index, local_P2P=true)
-    horizontal_pass_swapped!(tree, elements, 1, 1, theta, targets_index, sources_index, local_P2P)
-end
-
-function horizontal_pass_swapped!(tree, elements, i_target, j_source, theta, targets_index, sources_index, local_P2P=true)
-    branches = tree.branches
-    source_branch = branches[j_source]
-    target_branch = branches[i_target]
-    spacing = source_branch.center - target_branch.center
-    spacing_squared = spacing' * spacing
-    threshold_squared = (target_branch.radius + source_branch.radius)^2 * theta # theta is the number of radii squared
-    if target_branch.is_source
-        if spacing_squared >= threshold_squared # meet separation criteria
-            M2L!(tree, i_target, j_source)
-        elseif source_branch.first_branch == target_branch.first_branch == -1 && (local_P2P || i_target != j_source) # both leaves
-            P2P!(tree, elements, i_target, j_source, sources_index, targets_index)
-        elseif source_branch.first_branch == -1 || (target_branch.radius >= source_branch.radius && target_branch.first_branch != -1)
-            for i_child in target_branch.first_branch:target_branch.first_branch + target_branch.n_branches - 1
-                horizontal_pass_swapped!(tree, elements, i_child, j_source, theta, targets_index, sources_index, local_P2P)
-            end
-        else
-            for j_child in source_branch.first_branch:source_branch.first_branch + source_branch.n_branches - 1
-                horizontal_pass_swapped!(tree, elements, i_target, j_child, theta, targets_index, sources_index, local_P2P)
-            end
-        end
-    end
-end
-
-function downward_pass_swapped!(tree, elements, sources_index)
-    downward_pass!(tree, elements, 1, sources_index)
-end
-
-function downward_pass_swapped!(tree, elements, j_source, sources_index)
-    # expose branch
-    branch = tree.branches[j_source]
-
-    if branch.is_source
-        # if a leaf, perform L2P on
-        if branch.first_branch == -1 # leaf
-            # println("DP: L2B: $j_source")
-            L2B!(tree, elements, j_source, sources_index)
-        else # not a leaf, so perform L2L! and recurse
-            # println("DP: L2L: $j_source")
-            L2L!(tree, j_source)
-            for i_child in branch.first_branch:branch.first_branch + branch.n_branches - 1
-                downward_pass!(tree, elements, i_child, sources_index)
-            end
-        end
-    end
-end
-
-#####
 ##### running FMM
 #####
-function fmm!(tree::Tree, elements, options::Options; reset_tree=true, swap_source_target=false, local_P2P=true)
+function fmm!(tree::Tree, elements, options::Options, targets_index, sources_index; reset_tree=true, local_P2P=true)
     if reset_tree; reset_expansions!(tree); end
-    if swap_source_target
-        upward_pass_swapped!(tree, elements, options.targets_index)
-        horizontal_pass_swapped!(tree, elements, options.theta, options.targets_index, options.sources_index, local_P2P)
-        downward_pass_swapped!(tree, elements, options.sources_index)
-    else
-        upward_pass!(tree, elements, options.sources_index)
-        horizontal_pass!(tree, elements, options.theta, options.targets_index, options.sources_index, local_P2P)
-        downward_pass!(tree, elements, options.targets_index)
-    end
+    upward_pass!(tree, elements, sources_index)
+    horizontal_pass!(tree, elements, options.theta, targets_index, sources_index, local_P2P)
+    downward_pass!(tree, elements, targets_index)
 end
 
 """
@@ -214,6 +116,14 @@ The user must reset the potential manually.
 """
 function fmm!(elements::Tuple, options::Options; optargs...)
     tree = Tree(elements, options)
-    fmm!(tree, elements, options; optargs...)
+    full_index = collect(1:length(elements))
+    fmm!(tree, elements, options, full_index, full_index; optargs...)
+    return tree
+end
+
+function fmm!(elements::Tuple, options::Options, target_index, source_index; optargs...)
+    tree = Tree(elements, options)
+    full_index = collect(1:length(elements))
+    fmm!(tree, elements, options, target_index, source_index; optargs...)
     return tree
 end
