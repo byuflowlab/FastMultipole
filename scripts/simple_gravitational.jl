dad308c90f7a4bae1f9bda23709caa781a620e6f
Ryan Anderson <ryan@tmplab-32-117-123.et.byu.edu>
2023-09-12 10:26:57 -0600

    use BenchmarkTools

--- scripts/simple_gravitational.jl
+++ scripts/simple_gravitational.jl
@@ -1,6 +1,7 @@
 include("../test/gravitational.jl")
+using BenchmarkTools
 
-n_bodies = 30000
+n_bodies = 300000
 bodies = rand(8,n_bodies)
 systems = (Gravitational(bodies),)
 options = fmm.Options(13,700,10.0)
@@ -8,11 +9,11 @@ old_bodies = deepcopy(systems[1].bodies)
 tree = fmm.Tree(systems, options)
 # note: old_bodies[tree.index_list[1]] = systems[1].bodies
 println("Run FMM:")
-@time fmm.fmm!(tree, systems, options; unsort_bodies=true)
+@btime fmm.fmm!($tree, $systems, $options; unsort_bodies=true)
 println("done.")
 systems2 = (Gravitational(bodies),)
 println("Run direct:")
-@time fmm.direct!(systems2[1], 1:n_bodies, systems2[1], 1:n_bodies)
+@btime fmm.direct!(systems2[1], 1:n_bodies, systems2[1], 1:n_bodies)
 println("done.")
 phi = systems[1].potential[1,:]
 phi2 = systems2[1].potential[1,:]
