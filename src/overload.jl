38c01b50e050055df9d4fc9ae5c2a6c302104250
Ryan Anderson <ryan@tmplab-32-117-123.et.byu.edu>
2023-09-12 08:20:55 -0600

    finish overloading

--- src/overload.jl
+++ src/overload.jl
@@ -10,18 +10,23 @@
 # Base.getindex(sys, i, ::VelocityGradient) =
 # Base.getindex(sys, i) =
 function Base.setindex!(sys, val, i)
+    @warn "setindex! not overloaded for type $(typeof(sys))"
     return nothing
 end
 function Base.setindex!(sys, val, i, ::ScalarPotential)
+    @warn "setindex! (ScalarPotential) not overloaded for type $(typeof(sys))"
     return nothing
 end
 function Base.setindex!(sys, val, i, ::Potential)
+    @warn "setindex! (Potential) not overloaded for type $(typeof(sys))"
     return nothing
 end
 function Base.setindex!(sys, val, i, ::Velocity)
+    @warn "setindex! (Velocity) not overloaded for type $(typeof(sys))"
     return nothing
 end
 function Base.setindex!(sys, val, i, ::VelocityGradient)
+    @warn "setindex! (VelocityGradient) not overloaded for type $(typeof(sys))"
     return nothing
 end
 # Base.length(system) = 
