1d57b7e2c2d7ebc9c8befd80ac5c9faacfe746cd
Ryan Anderson <rymanderson@gmail.com>
2023-11-18 10:42:45 -0700

    bug fix

--- src/fmm.jl
+++ src/fmm.jl
@@ -165,8 +165,8 @@ function horizontal_pass_multi_thread!(branches, m2l_list, expansion_order)
 
     # divide and concquer
     Threads.@threads for i_start in 1:n_per_chunk:length(m2l_list)
-        harmonics = zeros(eltype(branches[i_start].multipole_expansion), (expansion_order<<1 + 1)*(expansion_order<<1 + 1))
-        L = zeros(eltype(branches[i_start].local_expansion), 4)
+        harmonics = zeros(eltype(branches[1].multipole_expansion), (expansion_order<<1 + 1)*(expansion_order<<1 + 1))
+        L = zeros(eltype(branches[1].local_expansion), 4)
         for (i_target, j_source) in view(m2l_list,i_start:i_start+n_per_chunk-1)
             target_branch = branches[i_target]
             Threads.@lock target_branch.lock M2L!(target_branch, branches[j_source], harmonics, L, expansion_order)
