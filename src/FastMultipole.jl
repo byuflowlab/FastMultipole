d8e791e6f1bc1e1fb53953befa3dd329cfe394f3
Ryan Anderson <rymanderson@gmail.com>
2024-08-31 14:41:25 -0600

    efficient rotation matrices

--- src/FastMultipole.jl
+++ src/FastMultipole.jl
@@ -13,9 +13,15 @@ const MIN_NPT_L2L = 100
 const MIN_NPT_L2B = 100
 const MIN_NPT_NF = 100
 
-for file in ["containers", "complex", "derivatives", "element", "tree", "direct", "spherical", "fmm", "sortwrapper", "compatibility", "b2m", "probe", "visualize"]#, "estimate_cost"]
+# preallocate y-axis rotation matrices by π/2
+const Hs_π2 = Float64[1.0]
+
+for file in ["containers", "complex", "derivatives", "element", "tree", "direct", "rotate", "spherical", "fmm", "sortwrapper", "compatibility", "b2m", "probe", "visualize"]#, "estimate_cost"]
     include(file*".jl")
 end
+
+# preallocate y-axis rotation matrices up to 20th order
+update_Hs_π2!(Hs_π2, 20)
 
 export fmm!, direct!, Tree, SortWrapper, ProbeSystem, add_line!, reset!, DerivativesSwitch
 
--- src/rotate.jl
+++ src/rotate.jl
@@ -1,6 +1,13 @@
+#--- precompute y axis rotation matrices by π/2 ---#
+
 function length_Hs(expansion_order)
     p = expansion_order
-    return div((p+1)*(p+2)*(2p+3), 6)
+    n = p+1
+    return div(n*(n+1)*(n+2), 6)
+end
+
+function length_H(n)
+    return ((n+1)*(n+2))>>1
 end
 
 function get_H(Hs, n)
@@ -14,7 +21,7 @@ function get_H(Hs, n)
 end
 
 @inline function H_index(mp, m)
-    return m^2 + mp + m + 1
+    return (m*(m+1))>>1 + mp + 1
 end
 
 @inline function Pnm_index(n, m)
@@ -56,11 +63,14 @@ function compute_Hs!(Hs, β, n_start, n_end)
     bnms = zeros(2(n_end+1)+1)
 
     # d
-    dnms = zeros(2*n_end+1)
+    dnms = zeros(n_end+1)
 
     # Hs
+    i_start = length_Hs(n_start-1) + 1
+    i_end = i_start + length_H(n_start) - 1
     for n in n_start:n_end
-        H = get_H(Hs, n)
+        #H = get_H(Hs, n)
+        H = view(Hs, i_start:i_end)
 
         # H_n_0m
         mp = 0
@@ -106,36 +116,24 @@ function compute_Hs!(Hs, β, n_start, n_end)
         end
 
         # update dnms
-        for m in -n:-1
-            dnms[n+m+1] = -sqrt((n-m)*(n+m+1)) / 2
-        end
         for m in 0:n
-            dnms[n+m+1] = sqrt((n-m)*(n+m+1)) / 2
+            dnms[m+1] = sqrt((n-m)*(n+m+1)) / 2
         end
 
         # H_n_mp+1_m
         for mp in 1:n-1
             for m in mp+1:n-1
-                val = dnms[n+mp] * H[H_index(mp-1,m)] - dnms[n+m] * H[H_index(mp,m-1)] + dnms[n+m+1] * H[H_index(mp,m+1)]
-                H[H_index(mp+1,m)] = val / dnms[n+mp+1]
+                val = dnms[mp] * H[H_index(mp-1,m)] - dnms[m] * H[H_index(mp,m-1)] + dnms[m+1] * H[H_index(mp,m+1)]
+                H[H_index(mp+1,m)] = val / dnms[mp+1]
             end
             m = n
-            val = dnms[n+mp] * H[H_index(mp-1,m)] - dnms[n+m] * H[H_index(mp,m-1)]
-            H[H_index(mp+1,m)] = val / dnms[n+mp+1]
+            val = dnms[mp] * H[H_index(mp-1,m)] - dnms[m] * H[H_index(mp,m-1)]
+            H[H_index(mp+1,m)] = val / dnms[mp+1]
         end
 
-        # H_n_mp-1_m
-        for mp in 0:-1:-n+1
-            for m in -mp+1:n-1
-                val = dnms[n+mp+1] * H[H_index(mp+1,m)] + dnms[n+m] * H[H_index(mp,m-1)] - dnms[n+m+1] * H[H_index(mp,m+1)]
-                H[H_index(mp-1,m)] = val / dnms[n+mp]
-                @show mp-1
-            end
-            m = n
-            val = dnms[n+mp+1] * H[H_index(mp+1,m)] + dnms[n+m] * H[H_index(mp,m-1)]
-            H[H_index(mp-1,m)] = val / dnms[n+mp]
-            @show mp-1
-        end
+        # recurse indices
+        i_start = i_end + 1
+        i_end = i_start + length_H(n+1) - 1
 
     end
 end
@@ -160,4 +158,124 @@ function update_Hs_π2!(expansion_order)
     end # otherwise, we already have enough, so do nothing
 end
 
-#update_Hs_π2!(Hs_π2, 20)
+#--- use precomputed H matrices to compute rotations for arbitrary angle ---#
+
+function length_Ts(expansion_order)
+    p = expansion_order
+    return div((p+1)*(p+2)*(2p+3),6)
+end
+
+function length_T(n)
+    return (n+1)^2
+end
+
+function get_T(Ts, n)
+    if n == 0
+        return view(Ts, 1:1)
+    else
+        i1 = length_Ts(n-1)+1
+        i2 = length_Ts(n)
+        return view(Ts,i1:i2)
+    end
+end
+
+@inline function T_index(mp, m)
+    return m^2 + mp + m + 1
+end
+
+@inline function get_scalar(m_mp)
+    isodd(m_mp) && (m_mp += 1)
+    if iseven(m_mp>>1)
+        return 1.0
+    else
+        return -1.0
+    end
+end
+
+function update_Ts!(Ts, β, expansion_order)
+    p = expansion_order
+
+    # initialize recursive indices
+    i_H_start = length_Hs(0) + 1 # 2
+    i_H_end = i_H_start + 2 # n=1 matrix contains 3 unique entries
+    i_T_start = 2
+    i_T_end = i_T_start + 3 # n=1 T matrix contains 4 unique entries
+
+    # other recursive values
+    sβ, cβ = sincos(β)
+    _1_n = -1
+
+    # rotate each expansion order
+    @inbounds for n in 1:p
+        # Wigner matrix about y axis by π/2
+        H_π2 = view(Hs_π2, i_H_start:i_H_end)
+
+        # Wigner matrix by β
+        T = view(Ts, i_T_start:i_T_end)
+
+        for m in 0:n
+            H_π2_n_m_0 = H_π2[H_index(0,m)]
+            _1_mp_odd = 1.0
+            m_mp = m
+            _1_n_mp = _1_n
+
+            for mp in 0:m
+                H_π2_n_mp_0 = H_π2[H_index(0,mp)]
+                val_positive_mp = zero(eltype(Ts))
+                val_negative_mp = zero(eltype(Ts))
+                s_νm1_β = 0.0
+                c_νm1_β = 1.0
+                m_mp_even = iseven(m_mp)
+                scalar = get_scalar(m_mp)
+                _1_n_mp_ν = -_1_n_mp
+
+                for ν in 1:n
+                    # sin and cos (νβ)
+                    c_νβ = cβ * c_νm1_β - sβ * s_νm1_β
+                    s_νβ = sβ * c_νm1_β + cβ * s_νm1_β
+
+                    # alternating sine/cosine
+                    sc_νβ_m_mp = m_mp_even ? scalar * c_νβ : scalar * s_νβ
+
+                    # accumulate
+                    i, j = minmax(mp,ν)
+                    H_π2_n_mp_ν = H_π2[H_index(i,j)]
+                    i, j = minmax(m,ν)
+                    H_π2_n_m_ν = H_π2[H_index(i,j)]
+
+                    # leverage symmetry to compute p/m mp
+                    val = H_π2_n_mp_ν * H_π2_n_m_ν * sc_νβ_m_mp
+                    val_positive_mp += val
+                    val_negative_mp += val * _1_n_mp_ν * _1_mp_odd
+
+                    # recurse
+                    c_νm1_β = c_νβ
+                    s_νm1_β = s_νβ
+                    _1_n_mp_ν = -_1_n_mp_ν
+                end
+
+                val_positive_mp *= 2.0
+                val_negative_mp *= 2.0
+                scalar = m_mp_even ? scalar : 0.0
+                val = H_π2_n_m_0 * H_π2_n_mp_0 * scalar
+                val_positive_mp += val
+                val_negative_mp += val * _1_n_mp * _1_mp_odd
+                T[T_index(mp,m)] = val_positive_mp
+                T[T_index(-mp,m)] = val_negative_mp
+
+                # recurse
+                _1_n_mp = -_1_n_mp
+                _1_mp_odd = -_1_mp_odd
+                m_mp += 1
+            end
+        end
+
+        # recurse
+        i_H_start = i_H_end + 1
+        i_H_end = i_H_start + length_H(n+1) - 1
+        i_T_start = i_T_end + 1
+        i_T_end = i_T_start + length_T(n+1) - 1
+        _1_n = -_1_n
+    end
+end
+
